26d4cb2e698c68f90f2bfea66c40b9342fa3994a
Miyako Matsui <miyako.matsui18@gmail.com>
2023-11-21 14:25:07 +1300

    translate to english

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ def validate(hand):
     return False
   return True
 
-def print_hand(hand, name='ゲスト'):
-    hands = ["グー","チョキ","パー"]
-    print(name + 'は' + hands[hand] + 'を出しました')
+def print_hand(hand, name='guest'):
+    hands = ["rock","scissors","paper"]
+    print(name + ' chose' + " "+hands[hand])
 
 def judge(player, computer):
   if player == computer:
-    return "引き分け"
+    return "draw"
 
   elif player == 0 and computer ==1:
-    return "勝ち"
+    return "win"
 
   elif player == 1 and computer ==2:
-    return "勝ち"
+    return "win"
 
   elif player == 2 and computer ==0:
-    return "勝ち"
+    return "win"
 
   else:
-    return "負け"
+    return "loose"
 
-print('じゃんけんをはじめます')
-player_name = input('名前を入力してください：')
-print("何を出しますか？（0: グー, 1: チョキ, 2: パー）")
+print('Play Rock, Scissors, Paper')
+player_name = input('Enter your name：')
+print("Choose（0: rock, 1: scissors, 2: paper）")
 
-player_hand = int(input("数字で入力してください："))
+player_hand = int(input("Enter number："))
 
 if validate(player_hand):
   computer_hand = 1
@@ -37,10 +37,10 @@ if validate(player_hand):
   else:
      print_hand(player_hand, player_name)
 
-  print_hand(computer_hand,"コンピューター")
+  print_hand(computer_hand,"computer")
 
   result =  judge(player_hand, computer_hand)
-  print(player_name + "の勝負の結果は" + result + "でした")
+  print(player_name + " " + result )
 
 else: 
-  print("正しい数値を入れてください")
+  print("Choose from 0 to 2")
