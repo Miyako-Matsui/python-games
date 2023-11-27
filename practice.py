5eae4b241fbc19499d6344381038f972fb35d411
Miyako Matsui <miyako.matsui18@gmail.com>
2023-11-27 14:29:37 +1300

    function

--- practice.py
+++ practice.py
@@ -1,12 +1,42 @@
-weight = int(input("Your Weight: "))
-unit = input(("Is (K)g or (L)bs: "))
-if unit.upper() == "K":
-   converted = weight / 0.45
-   print("Your in Lbs: " + str(converted))
-else:
-   converted = weight * 0.45
-   print("Your in Kg: " + str(converted))
+# weight = int(input("Your Weight: "))
+# unit = input(("Is (K)g or (L)bs: "))
+# if unit.upper() == "K":
+#    converted = weight / 0.45
+#    print("Your in Lbs: " + str(converted))
+# else:
+#    converted = weight * 0.45
+#    print("Your in Kg: " + str(converted))
 
+# numbers = range(5,10, 2)
+# for number in range(5):
+#  print(number)
+
+# i = 0 
+# while i < 10:
+#  i = i + 1
+#  if (i % 2 ) == 0:
+#   print(i)
+
+# numbers = [1,2,3,4,5]
+# for item in numbers:
+#   print(item)
+
+# i = 0
+# while i < len(numbers):
+#   print(numbers[i])
+#   i = i + 1
+
+# i = 1 
+# while i <= 5:
+#    print (i * "*")
+#    i = i + 1  
+
+# names = ["Mimi","Mina","Mithuko","Mint"]
+# print(names[0:3])
+# print(names)
+
+# numbers = [1,2,3]
+# print(len(numbers))
 
 # float(weight * 0.45)
 # int(weight * 2)
@@ -29,3 +59,45 @@ else:
 # patient_history = "new"
 
 # print("Name : " + name + " Age : " + str(age) + " Patient Histry :" + patient_history) 
+
+# Excercise
+# count =  0
+# for number in range(1,10):
+#   if (number % 2) == 0:
+#     count += 1 
+#     print(number)
+# print ("we have " + str(count))
+    
+# num_1 = 1 
+# num_2 = 2 
+# add = num_1 + num_2
+# print(add)
+
+# str_1 = "Hello, "
+# str_2 = "World!"
+# conbine_str = str_1 + str_2
+# print(conbine_str)
+
+# user_number = int(input("Enter any number : "))
+# if (user_number % 2 ) == 0:
+#     print("Your number is Even")
+# else:
+#     print("Your number is Odd")
+   
+# i = 1
+# while i <= 10:
+#   print(i)
+#   i = i + 1
+
+# for number in range(1,11):
+#  print(number) 
+
+# list = [1,2,3,4,5]
+# for item in list:
+#   result = item * 2
+#   print(result)
+
+def add(a,b):
+ return a + b 
+
+print(add(1,2))
